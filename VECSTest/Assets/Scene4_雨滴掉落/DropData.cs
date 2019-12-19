6ad55e6254ef03eab4c1e39e59092c3ef0708902
SmallHat <1031464180@qq.com>
2019-12-19 17:16:16 +0800

    UnityECS基础案例-Scene4更新
    Scene4更新

---
+++ VECSTest/Assets/Scene4_雨滴掉落/DropData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[System.Serializable]
+public struct DropData : IComponentData
+{
+    public float delay;
+    public float velocity;
+}
---
+++ VECSTest/Assets/Scene4_雨滴掉落/DropSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class DropSystem : ComponentSystem
+{
+    public float minHeight = -100;                              // 下落最低值
+    protected override void OnUpdate()
+    {
+        // 通过ECS提供的筛方式遍历场景中包含该数据组件的实体
+        Entities.ForEach((ref Translation translation, ref DropData dropData) =>
+        {
+
+            if (dropData.delay > 0)
+            {
+                dropData.delay -= Time.deltaTime;
+            }
+            else
+            {
+                if (translation.Value.y < minHeight)
+                {
+                    translation.Value.y = 0;
+                    dropData.velocity = Random.Range(1, 10);
+                    dropData.delay = Random.Range(1,10);
+                }
+                else
+                {
+                    translation.Value.y -= dropData.velocity * Time.deltaTime;
+                }
+            }
+        });
+    }
+}
