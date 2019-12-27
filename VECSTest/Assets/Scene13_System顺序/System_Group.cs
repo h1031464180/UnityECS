577671ffa87cb770c59fa8894be3cfd396942d3c
SmallHat <1031464180@qq.com>
2019-12-28 00:52:02 +0800

    System执行顺序
    System执行顺序

---
+++ VECSTest/Assets/Scene13_System顺序/System_Group.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class Enemy_Create : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class Enemy_Move : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[UpdateAfter(typeof(Enemy_Move))]
+public class Enemy_Attack : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
--- VECSTest/Assets/Scene4_Pure雨滴掉落/DropSystem.cs
+++ VECSTest/Assets/Scene4_Pure雨滴掉落/DropSystem.cs
@@ -12,7 +12,6 @@ public class DropSystem : ComponentSystem
         // 通过ECS提供的筛方式遍历场景中包含该数据组件的实体
         Entities.ForEach((ref Translation translation, ref DropData dropData) =>
         {
-
             if (dropData.delay > 0)
             {
                 dropData.delay -= Time.deltaTime;
@@ -23,7 +22,7 @@ public class DropSystem : ComponentSystem
                 {
                     translation.Value.y = 0;
                     dropData.velocity = Random.Range(1, 10);
-                    dropData.delay = Random.Range(1,10);
+                    dropData.delay = Random.Range(1, 10);
                 }
                 else
                 {
